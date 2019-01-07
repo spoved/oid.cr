6f8153eaae9647d8dc75aad91237638b680dfa94
Holden Omans <holden.omans@gmail.com>
2019-01-07 18:38:05 -0500

    Initial commit

---
+++ spec/oid_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Oid do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/oid.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Oid`
+module Oid
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
