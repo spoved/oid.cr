b51d434ab2b018b9e0e86809f25dd400f581d858
Holden Omans <holden.omans@gmail.com>
2019-05-22 09:31:59 -0400

    updates to build bindings

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -ex
+
+root_dir=$(pwd)
+
+cd_root() {
+  cd ${root_dir}
+}
+
+make_clean() {
+  cd ./ext
+  make clean
+  cd_root
+}
+
+# Update submodules
+git submodule update --init --recursive
+
+# Check bindgen
+if [ ! -e ./lib/bindgen/clang/bindgen ]; then
+  cd ./lib/bindgen/clang/
+  make
+  cd_root
+fi
+
+if [ ! -e ./lib/bindgen/bin/bindgen ]; then
+  cd ./lib/bindgen
+  shards build
+  cd_root
+fi
+
+# Compile raylib
+if [ ! -e ./ext/raylib/src/libraylib.a ]; then
+  cd ./ext/raylib/src/
+  make
+  cd_root
+fi
+
+# Generate bindings
+if [ ! -d ./src/ext ]; then
+  mkdir ./src/ext
+fi
+
+./lib/bindgen/bin/bindgen ext/raylib.yml
+
+cd_root
+
+crystal tool format
