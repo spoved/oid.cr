21e0a1efedb3b70e0131d17c00094b98d77fcfb3
Holden Omans <holden.omans@gmail.com>
2019-12-16 18:50:08 -0500

    update

--- src/oid/features/view/view_service.cr
+++ src/oid/features/view/view_service.cr
@@ -4,12 +4,8 @@ module Oid
       include Oid::Service
 
       # create a view from a premade asset (e.g. a prefab)
-      abstract def load_asset(
-        contexts : Contexts,
-        entity : Entity::IEntity,
-        asset_type : Oid::Enum::AssetType,
-        asset_name : String
-      )
+      abstract def load_asset(contexts : Contexts, entity : Entity::IEntity,
+                              asset_type : Oid::Enum::AssetType, asset_name : String)
     end
   end
 end
